9d2df51f795586a37efb1ac4ff8bd82ca8b94329
akira345 <g.akira345@gmail.com>
2017-07-06 00:48:37 +0900

    FormTypeで書き換え

--- src/Testapp/Controller/AdminController.php
+++ src/Testapp/Controller/AdminController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Testapp\Form\Type\Admin\LoginType;
 
 
 /**
@@ -27,17 +28,12 @@ Class AdminController
     public function login(Application $app, Request $request)
     {
 
-        $form = $app['form.factory']->createNamedBuilder('', FormType::class)
-            ->add('username',TextType::class,array(
-                    'data' =>$app['session']->get('_security.last_username')  //値や属性をセット。Twig側でも出来る。
-                )
-            )
-            ->add('password',PasswordType::class)
+        $form = $app['form.factory']->createNamedBuilder('', LoginType::class)
             ->getForm();
 
         return $app['twig']->render('Admin/login.twig', array(
             'error'         => $app['security.last_error']($request),
-            'last_username' => $app['session']->get('_security.last_username'),
+            //'last_username' => $app['session']->get('_security.last_username'),
             'form' => $form->createView(),
         ));
 
