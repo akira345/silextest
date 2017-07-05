e18663ee50ca2b72f6fafeb6312b02ee85643738
akira345 <g.akira345@gmail.com>
2017-07-05 23:27:32 +0900

    TopコントローラをHelloコントローラへ名称変更。

--- src/Testapp/ControllerProvider/FrontControllerProvider.php
+++ src/Testapp/ControllerProvider/FrontControllerProvider.php
@@ -19,7 +19,7 @@ class FrontControllerProvider implements ControllerProviderInterface
         $c = $app['controllers_factory'];
         //ルーティング
         $c->match('/','\Testapp\Controller\DefaultController::index');
-        $c->match('/hello/{name}','\Testapp\Controller\TopController::index');
+        $c->match('/hello/{name}','\Testapp\Controller\HelloController::index');
 
         $c->match('/admin/','\Testapp\Controller\AdminController::index');
         $c->match('/admin/login','\Testapp\Controller\AdminController::login');
